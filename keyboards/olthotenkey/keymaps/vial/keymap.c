ddea9671aeebe6e1d77cad0f3d307ea3ef3a0a19
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2025-12-03 15:41:01 +0900

    Update keymap.c

--- keyboards/olthotenkey/keymaps/vial/keymap.c
+++ keyboards/olthotenkey/keymaps/vial/keymap.c
@@ -7,33 +7,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    
     [0] = LAYOUT(
          KC_BSPC, TG(1),   KC_PSLS, KC_PAST,  KC_PMNS,
-         KC_DEL,  KC_P7,   KC_P8,   KC_P9,
+         KC_DEL,  KC_P7,   KC_P8,   KC_P9,    KC_NO,
          KC_UP,   KC_P4,   KC_P5,   KC_P6,    KC_PPLS,
-         KC_DOWN, KC_P1,   KC_P2,   KC_P3,
+         KC_DOWN, KC_P1,   KC_P2,   KC_P3,    KC_NO,
          KC_LEFT, KC_RGHT, KC_P0,   KC_PDOT,  KC_PENT
 
     ),
     [1] = LAYOUT(
-         KC_TAB,  TG(1),   KC_F2,   KC_BSPC,  KC_DEL,
-         KC_Q,    KC_W,    KC_E,    KC_R,
+         KC_TAB,  TG(1),   KC_F2,   KC_BSPC, KC_DEL,
+         KC_Q,    KC_W,    KC_E,    KC_R,    KC_NO,
          KC_A,    KC_S,    KC_D,    KC_F,    KC_T,
-         KC_Z,    KC_X,    KC_C,    KC_V, 
+         KC_Z,    KC_X,    KC_C,    KC_V,    KC_NO,
          KC_LCTL, KC_INT5, KC_P0,   KC_SPC,  KC_G
 
     ),
     [2] = LAYOUT(
          KC_BSPC, TG(1),   KC_PSLS, KC_PAST,  KC_PMNS,
-         KC_DEL,  KC_P7,   KC_P8,   KC_P9,
+         KC_DEL,  KC_P7,   KC_P8,   KC_P9,    KC_NO,
          KC_UP,   KC_P4,   KC_P5,   KC_P6,    KC_PPLS,
-         KC_DOWN, KC_P1,   KC_P2,   KC_P3,
+         KC_DOWN, KC_P1,   KC_P2,   KC_P3,    KC_NO,
          KC_LEFT, KC_RGHT, KC_P0,   KC_PDOT,  KC_PENT
 
     ),
     [3] = LAYOUT(
          KC_BSPC, TG(1),   KC_PSLS, KC_PAST,  KC_PMNS,
-         KC_DEL,  KC_P7,   KC_P8,   KC_P9,
+         KC_DEL,  KC_P7,   KC_P8,   KC_P9,    KC_NO,
          KC_UP,   KC_P4,   KC_P5,   KC_P6,    KC_PPLS,
-         KC_DOWN, KC_P1,   KC_P2,   KC_P3,
+         KC_DOWN, KC_P1,   KC_P2,   KC_P3,    KC_NO,
          KC_LEFT, KC_RGHT, KC_P0,   KC_PDOT,  KC_PENT
 
     )
