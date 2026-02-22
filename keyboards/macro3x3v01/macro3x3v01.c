fb0512c36d2ba99508a3f7d845229ca19945d095
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-02-22 23:35:20 +0900

    Update macro3x3v01.c

--- keyboards/macro3x3v01/macro3x3v01.c
+++ keyboards/macro3x3v01/macro3x3v01.c
@@ -1,1 +1,17 @@
 #include "macro3x3v01.h"
+
+#define LAYOUT( \
+    k00, k01, k02, \
+    k10, k11, k12, \
+    k20, k21, k22, \
+         k30,       \
+         k40        \
+) { \
+    { k00, k01, k02 }, \
+    { k10, k11, k12 }, \
+    { k20, k21, k22 }, \
+    { k30, KC_NO, KC_NO }, \
+    { KC_NO, k40, KC_NO } \
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {};
