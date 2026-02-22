853e8c3cf54fe27f2bc7f4bb229508dc2a8e9807
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-02-23 00:52:07 +0900

    Update keymap.c

--- keyboards/macro3x3v01/keymaps/vial/keymap.c
+++ keyboards/macro3x3v01/keymaps/vial/keymap.c
@@ -15,23 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 /* エンコーダ処理 */
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch (index) {
-        case 0: // 左エンコーダ
-            if (clockwise) {
-                tap_code(KC_VOLU);
-            } else {
-                tap_code(KC_VOLD);
-            }
-            break;
-
-        case 1: // 右エンコーダ
-            if (clockwise) {
-                tap_code(KC_PGDN);
-            } else {
-                tap_code(KC_PGUP);
-            }
-            break;
-    }
-    return true;
-}
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = {
+        { KC_VOLU, KC_VOLD },   // encoder 0
+        { KC_PGDN, KC_PGUP }    // encoder 1
+    },
+};
+#endif
