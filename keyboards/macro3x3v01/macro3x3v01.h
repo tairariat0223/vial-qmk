d0f310aed686c0ece955d5a62c2f80e6c7cc01dd
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-02-22 23:22:14 +0900

    Update macro3x3v01.h

--- keyboards/macro3x3v01/macro3x3v01.h
+++ keyboards/macro3x3v01/macro3x3v01.h
@@ -1,18 +1,3 @@
 #pragma once
 
 #include "quantum.h"
-
-#define LAYOUT( \
-    k00, k01, k02, \
-    k10, k11, k12, \
-    k20, k21, k22, \
-         k30,       \
-         k40        \
-) \
-{ \
-    { k00, k01, k02 }, \
-    { k10, k11, k12 }, \
-    { k20, k21, k22 }, \
-    { k30, KC_NO, KC_NO }, \
-    { KC_NO, k40, KC_NO } \
-}
