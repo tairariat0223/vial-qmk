c8764273ee27a2497a523d0bbf8b6d8ff318d32e
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-02-24 20:05:31 +0900

    Update config.h

--- keyboards/macro3x3v01/config.h
+++ keyboards/macro3x3v01/config.h
@@ -44,7 +44,7 @@
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_INTERVAL       16
 #define MOUSEKEY_MOVE_DELTA     8
-#define MOUSEKEY_MAX_SPEED      6
+#define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_TIME_TO_MAX    64
 
 #define MOUSEKEY_WHEEL_DELAY    0
