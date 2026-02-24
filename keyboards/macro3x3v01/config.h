c6cbde5d7f4601dfc5862f3460ee0d066edc681a
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-02-24 20:04:40 +0900

    Update config.h

--- keyboards/macro3x3v01/config.h
+++ keyboards/macro3x3v01/config.h
@@ -15,8 +15,8 @@
 
 /* direct pins */
 #define DIRECT_PINS { \
-    { GP29, GP14, GP13 }, \
-    { GP28, GP8, GP26 }, \
+    { GP29, GP14, GP15 }, \
+    { GP28, GP8 , GP26 }, \
     { GP12, GP11, GP9 } , \
     { GP10,  NO_PIN, NO_PIN }, \
     { NO_PIN, GP27, NO_PIN }  \
