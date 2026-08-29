35a00a4a3e28e1f9a5616bb028df877fd4ff2ffe
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-08-29 19:38:03 +0900

    Comment out second layer in keymap.c
    Commented out the second layer of the keymap.

--- keyboards/chotten/keymaps/vial/keymap.c
+++ keyboards/chotten/keymaps/vial/keymap.c
@@ -11,6 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
          KC_P1,  KC_P2,  KC_P3,  KC_ENT,
          KC_0,   KC_PCMM,KC_PDOT,KC_CALC
+/*
     ),
     [1] = LAYOUT(
          KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
@@ -46,6 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
          KC_P1,  KC_P2,  KC_P3,  KC_ENT,
          KC_0,   KC_PCMM,KC_PDOT,KC_ENT
+*/
     )
 }
 ;
