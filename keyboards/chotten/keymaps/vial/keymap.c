3a8a8a51be2e1dc6164d156c6b9476ecad86191d
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-08-29 19:31:39 +0900

    Refactor keymap layout to use LAYOUT macro

--- keyboards/chotten/keymaps/vial/keymap.c
+++ keyboards/chotten/keymaps/vial/keymap.c
@@ -5,47 +5,47 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    
-    [0] = LAYOUT_all(
-         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-         KC_P7,  KC_P8,   KC_P9,   KC_PEQL,
-         KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-         KC_P1,  KC_P2,   KC_P3,   KC_ENT,
-         KC_0,   KC_PCMM, KC_PDOT, KC_CALC
+    [0] = LAYOUT(
+         KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
+         KC_P7,  KC_P8,  KC_P9,  KC_PEQL,
+         KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
+         KC_P1,  KC_P2,  KC_P3,  KC_ENT,
+         KC_0,   KC_PCMM,KC_PDOT,KC_CALC
     ),
-    [1] = LAYOUT_all(
-         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-         KC_P7,  KC_P8,   KC_P9,   KC_PEQL,
-         KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-         KC_P1,  KC_P2,   KC_P3,   KC_ENT,
-         KC_0,   KC_PCMM, KC_PDOT, KC_CALC
+    [1] = LAYOUT(
+         KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
+         KC_P7,  KC_P8,  KC_P9,  KC_PEQL,
+         KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
+         KC_P1,  KC_P2,  KC_P3,  KC_ENT,
+         KC_0,   KC_PCMM,KC_PDOT,KC_ENT
     ),
-    [2] = LAYOUT_all(
-         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-         KC_P7,  KC_P8,   KC_P9,   KC_PEQL,
-         KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-         KC_P1,  KC_P2,   KC_P3,   KC_ENT,
-         KC_0,   KC_PCMM, KC_PDOT, KC_CALC
-
+    [2] = LAYOUT(
+         KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
+         KC_P7,  KC_P8,  KC_P9,  KC_PEQL,
+         KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
+         KC_P1,  KC_P2,  KC_P3,  KC_ENT,
+         KC_0,   KC_PCMM,KC_PDOT,KC_ENT
     ),
-    [3] = LAYOUT_all(
-         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-         KC_P7,  KC_P8,   KC_P9,   KC_PEQL,
-         KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-         KC_P1,  KC_P2,   KC_P3,   KC_ENT,
-         KC_0,   KC_PCMM, KC_PDOT, KC_CALC
+    [3] = LAYOUT(
+         KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
+         KC_P7,  KC_P8,  KC_P9,  KC_PEQL,
+         KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
+         KC_P1,  KC_P2,  KC_P3,  KC_ENT,
+         KC_0,   KC_PCMM,KC_PDOT,KC_ENT
     ),
-    [4] = LAYOUT_all(
-         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-         KC_P7,  KC_P8,   KC_P9,   KC_PEQL,
-         KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-         KC_P1,  KC_P2,   KC_P3,   KC_ENT,
-         KC_0,   KC_PCMM, KC_PDOT, KC_CALC
+    [4] = LAYOUT(
+         KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
+         KC_P7,  KC_P8,  KC_P9,  KC_PEQL,
+         KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
+         KC_P1,  KC_P2,  KC_P3,  KC_ENT,
+         KC_0,   KC_PCMM,KC_PDOT,KC_ENT
     ),
-    [5] = LAYOUT_all(
-         KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-         KC_P7,  KC_P8,   KC_P9,   KC_PEQL,
-         KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
-         KC_P1,  KC_P2,   KC_P3,   KC_ENT,
-         KC_0,   KC_PCMM, KC_PDOT, KC_CALC
+    [5] = LAYOUT(
+         KC_NUM, KC_PSLS,KC_PAST,KC_PMNS,
+         KC_P7,  KC_P8,  KC_P9,  KC_PEQL,
+         KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
+         KC_P1,  KC_P2,  KC_P3,  KC_ENT,
+         KC_0,   KC_PCMM,KC_PDOT,KC_ENT
     )
-};
+}
+;
