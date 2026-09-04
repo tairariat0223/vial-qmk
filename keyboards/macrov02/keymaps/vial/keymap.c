76b9d117a67fd52f1fb47b4501a8207ff99bff13
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-09-04 18:09:09 +0900

    Fix formatting issue in keymap.c

--- keyboards/macrov02/keymaps/vial/keymap.c
+++ keyboards/macrov02/keymaps/vial/keymap.c
@@ -7,7 +7,7 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_BSPC, KC_UP  , KC_DEL,
-        KC_LEFT, KC_DOWN, KC_RGHT,
+        KC_LEFT, KC_DOWN, KC_RGHT
         )
 };
 
