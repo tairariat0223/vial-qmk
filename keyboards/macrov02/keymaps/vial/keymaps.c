ea3f947dc1715e2316d16f8affd1f6c6b629d2ad
tairariat0223 <222378095+tairariat0223@users.noreply.github.com>
2026-09-04 17:44:04 +0900

    Add initial keymap configuration for macrov02

---
+++ keyboards/macrov02/keymaps/vial/keymaps.c
@@ -0,0 +1,22 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        KC_BSPC, KC_UP  , KC_DEL,
+        KC_LEFT, KC_DOWN, KC_RGHT,
+        )
+};
+
+/* エンコーダ処理 */
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = {
+        { KC_RGHT, KC_LEFT },   // encoder 0
+        { KC_DOWN, KC_UP   }    // encoder 1
+    },
+};
+#endif
